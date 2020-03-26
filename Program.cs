052843f1d4092e521cca9f57c9a0fa36a9dedcb6
Suwath Ch <suwatch@microsoft.com>
2020-03-26 12:16:47 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppNetCore22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
